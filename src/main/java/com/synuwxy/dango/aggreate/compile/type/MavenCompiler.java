ab5f1bfa1328b68057552b124636a114349b3582
wxy <synuwxy@163.com>
2020-08-20 14:36:25 +0800

    先写一点点，后面再完善 先判断是否是maven构建的Java项目

--- src/main/java/com/synuwxy/dango/aggreate/compile/type/MavenCompiler.java
+++ src/main/java/com/synuwxy/dango/aggreate/compile/type/MavenCompiler.java
@@ -1,4 +1,45 @@
 package com.synuwxy.dango.aggreate.compile.type;
 
-public class MavenCompiler {
+import com.synuwxy.dango.aggreate.code.Language;
+import com.synuwxy.dango.aggreate.compile.Compiler;
+
+import java.io.File;
+
+/**
+ * @author wxy
+ */
+public class MavenCompiler implements Compiler {
+
+    @Override
+    public boolean belongs(Language language) {
+        return language.equals(Language.JAVA);
+    }
+
+    @Override
+    public boolean confirm(String root) {
+        File dir = new File(root);
+        if (!dir.isDirectory()) {
+            return false;
+        }
+        File[] listFiles = dir.listFiles();
+        if (null == listFiles) {
+            return false;
+        }
+
+        String keyName = "pom.xml";
+        for (File file : listFiles) {
+            if (file.isDirectory()) {
+                continue;
+            }
+            if (keyName.equals(file.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void compile() {
+
+    }
 }
